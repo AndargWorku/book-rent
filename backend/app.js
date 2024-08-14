92427f1d4d42427fdca055d5474167a3bed9b689
Andarg01 <workuandarg01@gmail.com>
2024-08-14 01:32:57 -0700

    update the route system

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
+      "https://book-rent-x75w.vercel.app/",
+      "https://book-rent-x75w.vercel.app",
       "http://localhost:5173",
       "http://localhost:5173/",
     ],
